d7735e20d671660a07af081e4e9221ccdfaccc23
DEVENDRA-MANI-TRIPATHI <92941880+DEVENDRA-MANI-TRIPATHI@users.noreply.github.com>
2025-08-16 22:05:58 +0530

    database connection and userModel

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,75 @@
+import mongoose, { Schema } from 'mongoose';
+import jwt from "jsonwebtoken"
+import bcrypt from "bcrypt"
+
+
+const UserSchema = new Schema(
+  {
+        username: {
+          type: String,
+          required: true,
+          trim: true,
+          minlength: 2,
+          maxlength: 20
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true
+        },
+        password: {
+            type: String,
+            required: true
+        },
+        role: {
+            type: String,
+            enum: ['user', 'admin'],
+            default: 'user'
+        },
+        refreshToken: {
+            type:String
+        }
+  },
+    {
+        timestamps: true
+    }
+);
+
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            email: this.email,
+            username: this.username
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+        }
+    )
+}
+
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+        }
+    )
+ }
+
+
+export default mongoose.model('User', UserSchema);
